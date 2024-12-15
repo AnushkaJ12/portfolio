2ecced4a7f1b214f34c722db0babcc8cd59c4212
AnushkaJ12 <147900511+AnushkaJ12@users.noreply.github.com>
2024-12-16 01:52:59 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,7 @@
+
+const linkedinButton = document.getElementById('linkedin-button');
+linkedinButton.href = "https://www.linkedin.com/in/anushka-jharimunya-a8aa5b235/";
+
+// Add GitHub URL dynamically
+const githubButton = document.getElementById('github-button');
+githubButton.href = "https://github.com/AnushkaJ12";
